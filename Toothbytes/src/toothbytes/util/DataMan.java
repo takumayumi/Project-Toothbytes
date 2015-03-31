72d0738851a72b96cfcf10a2349afcc7ee980057
Jolas Burayag <jolasburayag@gmail.com>
2015-03-31 08:34:08 +0800

    Optimized getAge method

--- Toothbytes/src/toothbytes/util/DataMan.java
+++ Toothbytes/src/toothbytes/util/DataMan.java
@@ -5,6 +5,8 @@
  */
 package toothbytes.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -12,7 +14,11 @@ import java.util.Date;
  * @author Jolas
  */
 public class DataMan {
-    public static Date getAge(Date bday) {
-        return new Date(System.currentTimeMillis() - bday.getTime());
+    public static int getAge(Calendar bday) {
+        long milis = System.currentTimeMillis() - bday.getTime().getTime();
+        return (int) (milis/1000/31536000);
+    }
+    public static String dateToString(Date d) {
+        return d.toString();
     }
 }
