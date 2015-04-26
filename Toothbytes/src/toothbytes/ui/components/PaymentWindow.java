9ed16f68e109a74ff78d9387bd04b8ea062629f6
RemmiG <remmigonzales@gmail.com>
2015-04-27 01:29:17 +0800

    Billing 0.5 //UI keeps going to the right //Patient search is not K //Transaction hasn't connected to the database yet

--- Toothbytes/src/toothbytes/ui/components/PaymentWindow.java
+++ Toothbytes/src/toothbytes/ui/components/PaymentWindow.java
@@ -6,7 +6,12 @@
 package toothbytes.ui.components;
 
 import java.util.ArrayList;
+import javax.swing.SwingUtilities;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import toothbytes.database.DBAccess;
 import toothbytes.model.Patient;
+import toothbytes.model.PatientX;
 
 /**
  *
@@ -15,12 +20,48 @@ import toothbytes.model.Patient;
 public class PaymentWindow extends ModuleWindow{
     private PatientListViewer plv;
     private PaymentViewer pv;
+    private int currentId;
     
     public PaymentWindow(ArrayList<Patient> pList) {
         plv = new PatientListViewer(pList);
-        pv = new PaymentViewer();
+        
+        PatientListListener pll = new PatientListListener();
+        plv.setListListener(pll);
+        
+        currentId = 1;
+        pv = new PaymentViewer(currentId);
         
         this.addToMainPane(pv);
         this.addPLV(plv);
+    }
+    
+    public void ChangePaymentViewer(){
+        pv.removeAll();
+        this.remove(pv);
+        pv = new PaymentViewer(currentId);
+        this.addToMainPane(pv);
+        SwingUtilities.updateComponentTreeUI(pv);
+        repaint();
+        revalidate();
+    }
+    
+    public class PatientListListener implements ListSelectionListener {
+
+        @Override
+        public void valueChanged(ListSelectionEvent e) {
+            try{
+                if (!e.getValueIsAdjusting()) {
+                    if(plv.getSelectedPatient().getId() != currentId){
+                        currentId = plv.getSelectedPatient().getId();
+                        ChangePaymentViewer();
+                    }
+                }
+            }catch(Exception ex){
+                System.out.println(plv.getSelectedPatient().getId());
+                System.out.println(ex);
+            }
+            
+        }
+
     }
 }
