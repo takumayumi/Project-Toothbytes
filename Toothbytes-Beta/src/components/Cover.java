559150fdeec862c086bfbf59a91d02cb68645ede
Kimwell Tandoc <kimwell.tandoc@gmail.com>
2015-06-24 11:37:58 -0700

    Landing page and registration validation update. -vil,jhe

--- Toothbytes-Beta/src/components/Cover.java
+++ Toothbytes-Beta/src/components/Cover.java
@@ -7,9 +7,14 @@ package components;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Font;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 /**
  * <h1>Cover</h1>
@@ -24,6 +29,24 @@ public class Cover extends JPanel{
         this.setLayout(new BorderLayout());
         this.setBackground(Color.white);
         JLabel logo = new JLabel(new ImageIcon("res/icons/main_logo.png"));
+        
+        DateFormat dateFormat = new SimpleDateFormat("MMMM dd, yyyy");
+        DateFormat timeFormat = new SimpleDateFormat("HH:mm a");
+        
+        Calendar cal = Calendar.getInstance();
+        JLabel test = new JLabel("Today is "  +dateFormat.format(cal.getTime()));
+        JLabel time = new JLabel(timeFormat.format(cal.getTime()));
+        
+        test.setFont(new Font("Arial", Font.PLAIN, 50));
+        test.setHorizontalAlignment(SwingConstants.CENTER);
+        test.setForeground(Color.DARK_GRAY);
+       
+        time.setFont(new Font("Arial", Font.PLAIN, 50));
+        time.setHorizontalAlignment(SwingConstants.CENTER);
+        time.setForeground(Color.DARK_GRAY);
+        
+        this.add(time, BorderLayout.SOUTH);
+        this.add(test, BorderLayout.NORTH);
         this.add(logo, BorderLayout.CENTER);
     }
     
